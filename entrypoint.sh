71681d5f828a4915e8863f48cb4a6fde5feb856c
Denis Matakov <denis@matakov.com>
2021-07-02 11:55:47 +0300

    Run Celery and Celery Beat in background

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,9 +13,12 @@ fi
 
 python manage.py collectstatic --noinput
 
-gunicorn _settings.wsgi:application --bind 0.0.0.0:8000
 echo "Connecting Celery..."
-celery -A _settings worker -l INFO
+celery -A _settings worker -l INFO &
+celery -A _settings beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler &
 echo "Celery connected!"
+
+gunicorn _settings.wsgi:application --bind 0.0.0.0:8000
+
 
 exec "$@"
